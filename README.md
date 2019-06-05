4a85236e7a8310d51bdaab1435c4e89622eed69b
Jagtap <vjagtap2@illinois.edu>
2019-06-05 14:47:30 -0500

    Unity stuff

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Unity_SampleScenes
 Unity Sample Scenes. Playing around with lightning and reflection in Unity 3D
+
+Hi, 
+
+This is just a sample pieces made with Unity 3D. This is just me messing around with lighting and reflection on 3D objects. 
+This was done through tutorials by LMHPoly on his youtube channel!
+Have fun exploring! XD
---
+++ VR game/VR Game/Assets/ShootingIfGrabbed.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShootingIfGrabbed : MonoBehaviour {
+
+    private SimpleShoot simpleShoot;
+    private OVRGrabbable ovrGrabbale;
+    public OVRInput.Button shootingButton;
+    public int maxNumberOfBullets = 15;
+    public Text bulletText;
+
+	// Use this for initialization
+	void Start () {
+        simpleShoot = GetComponent < SimpleShoot>();
+        ovrGrabbale = GetComponent<OVRGrabbable>();
+        bulletText.text = maxNumberOfBullets.ToString();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(ovrGrabbale.isGrabbed && OVRInput.GetDown(shootingButton, ovrGrabbale.grabbedBy.GetController()) && maxNumberOfBullets > 0)
+        {
+            //Shoot!!
+            simpleShoot.TriggerShoot();
+            maxNumberOfBullets--;
+            bulletText.text = maxNumberOfBullets.ToString();
+                
+        }
+	}
+}
